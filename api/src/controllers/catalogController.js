53a649613e0f066cd55f16f933ca2704cf369774
yavorkoen <yavorkoen@gmail.com>
2022-06-09 16:09:15 +0300

    add details page

--- api/src/controllers/catalogController.js
+++ api/src/controllers/catalogController.js
@@ -4,12 +4,18 @@ const catalogService = require('../services/catalogService.js');
 router.get('/', async(req, res) => {
     let allData = await catalogService.getAll();
     res.json(allData);
-})
+});
 
 router.post('/', (req, res) => {
     let furnitureData = req.body;
     catalogService.create(furnitureData);
     res.json({ok: true});
+});
+
+router.get('/:furnitureId', async (req, res) => {
+    let id = req.params.furnitureId;
+    let furniture = await catalogService.getOne(id);
+    res.json(furniture);
 })
 
 
--- api/src/services/catalogService.js
+++ api/src/services/catalogService.js
@@ -1,9 +1,8 @@
 const Furniture = require('../models/Furniture.js');
 
-exports.create = async (data) => {
-    return Furniture.create(data);
-}
-
+exports.create = async (data) => Furniture.create(data);
 
 exports.getAll = () => Furniture.find();
+
+exports.getOne = (id) => Furniture.findById(id);
 
